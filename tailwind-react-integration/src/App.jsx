a13539b5fb679a8f22473059436e7e7ad31fdd8a
magnusimam <imammagnus40@gmail.com>
2025-11-30 06:27:23 -0800

    commit

---
+++ tailwind-react-integration/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-500 to-purple-600">
+      <h1 className="text-4xl font-bold text-white">Tailwind CSS is working!</h1>
+    </div>
+  )
+}
